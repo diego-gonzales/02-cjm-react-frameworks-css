4f7fdd0a11540d8f7ce251abbfd2334f7411eff2
Diego Gonzales <diegoegj14@gmail.com>
2022-06-27 16:58:02 -0500

    Usando bootstrap y bulma mediante CDN

---
+++ src/components/01-Bootstrap.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./NavbarBootstrap";
+
+const Bootstrap = () => {
+  return (
+    <>
+      <h2>Bootstrap</h2>
+      <Navbar />
+    </>
+  );
+};
+
+export default Bootstrap;
---
+++ src/components/02-Bulma.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./CardBulma"
+
+const Bulma = () => {
+  return (
+    <>
+      <h2>Bulma</h2>
+
+      <Card />
+    </>
+  );
+};
+
+export default Bulma;
